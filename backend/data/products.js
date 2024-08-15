44a553719b214946946b1f897a58eea59ea1ceae
Safal Aryal <safalaryalofficial@gmail.com>
2024-08-15 21:55:12 +0930

    seeder file created, tested to be working

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,11 +1,10 @@
 const products = [
   {
-    _id: "1",
     name: "Airpods Wireless Bluetooth Headphone",
     image: "/images/airpods.jpg",
     description:
       "Airpods Wireless Bluetooth Headphones Bluetooth. Alow immense listening experience",
-    Brand: "Apple",
+    brand: "Apple",
     category: "Electronics",
     price: 89.99,
     countInStock: 10,
@@ -13,12 +12,11 @@ const products = [
     numReviews: 12,
   },
   {
-    _id: "2",
     name: "Alexa",
     image: "/images/alexa.jpg",
     description:
       "Airpods Wireless Bluetooth Headphones Bluetooth. Alow immense listening experience",
-    Brand: "Apple",
+    brand: "Apple",
     category: "Electronics",
     price: 89.99,
     countInStock: 10,
@@ -26,12 +24,11 @@ const products = [
     numReviews: 12,
   },
   {
-    _id: "3",
     name: "Camera",
     image: "/images/camera.jpg",
     description:
       "Airpods Wireless Bluetooth Headphones Bluetooth. Alow immense listening experience",
-    Brand: "Apple",
+    brand: "Apple",
     category: "Electronics",
     price: 819.99,
     countInStock: 10,
@@ -39,12 +36,11 @@ const products = [
     numReviews: 12,
   },
   {
-    _id: "4",
     name: "Mouse",
     image: "/images/mouse.jpg",
     description:
       "Airpods Wireless Bluetooth Headphones Bluetooth. Alow immense listening experience",
-    Brand: "google",
+    brand: "google",
     category: "Electronics",
     price: 40,
     countInStock: 10,
@@ -52,12 +48,11 @@ const products = [
     numReviews: 5,
   },
   {
-    _id: "5",
     name: "Phone",
     image: "/images/phone.jpg",
     description:
       "Airpods Wireless Bluetooth Headphones Bluetooth. Alow immense listening experience",
-    Brand: "Apple",
+    brand: "Apple",
     category: "Electronics",
     price: 1189.99,
     countInStock: 10,
